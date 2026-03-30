c460570d413d373c247f090c649239ba297296a7
sifti-grover <groversifti@gmail.com>
2026-03-30 21:13:39 +0530

    Day 1: Angular skeleton and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Landing } from './components/landing/landing';
+import { Login } from './components/login/login';
+import { Signup } from './components/signup/signup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // When the URL is exactly localhost:4200/ (empty path), show the Landing Page
+  { path: '', component: Landing },
+  
+  // When the URL is localhost:4200/login, show the Login Page
+  { path: 'login', component: Login },
+
+  { path: 'signup', component: Signup }
+];
